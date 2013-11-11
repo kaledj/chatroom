1901c51f54bf38632c53257f89d0672d1cfa375e
STashakkori <st79375@appstate.edu>
2013-11-11 16:00:37 -0500

    Update chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python      
+#!/usr/bin/python                                                                                                                             
 
 from socket import *
 import re
@@ -7,60 +7,76 @@ import sys
 
 class ChatClient:
 
-	userName = ""
-	serverPort = 0
-	users = []
+    userName = ""
+    serverPort = 0
 
-	# constructor
-	def __init__(self, serverPort):
-		self.serverPort = serverPort
+    # constructor                                                                                                                             
+    def __init__(self, serverPort):
+        self.serverPort = serverPort
 
-	# run 
-	#def run(self):
-	#    for arg in sys.argv:
-	#        process_command(arg)
+    # handle_connection                                                                                                                       
+    def send_username(self, serverPort):
+        request = connectionSocket.recv(1024)
+        regex = re.compile(request)
 
-	#    return    
-		#serverSocket = socket(AF_INET,SOCK_STREAM)
-		#serverSocket.bind(('',self.serverPort))
-		#serverSocket.listen(5)
-		#print ("Server ready for chat clients")
-		#while(1):
-		#    connectionSocket, addr  = serverSocket.accept()
-		#    clientSockets.append(connectionSocket)
-		#    thread.start_new_thread(self.handle_connection,(connectionSocket,))
-	 
-	# handle_connection  
-	def send_username(self, serverPort):
-		request = connectionSocket.recv(1024)
-		regex = re.compile(request)
-		
-	def process_command(self,command):   
-		if not command:
-			print "Error, username not provided. Program terminated"
-			sys.exit()
-		else:
-			self.userName = command
-		return
-			
-	def run(self):
-		self.process_command(sys.argv[1])
-		serverSocket = socket(AF_INET,SOCK_STREAM)
-		serverSocket.connect(('localhost',self.serverPort))
-		serverSocket.send("NAME " + self.userName)
-		status = serverSocket.recv(256)           
-		if status == "OK":
-			print "ok to chat"
-			
-		else:
-			print status
+    def process_command(self,command):
+        if not command:
+            print "Error, username not provided. Program terminated"
+            sys.exit()
+        else:
+            self.userName = command
 
-		serverSocket.send("USERS")
-		users = serverSocket.recv(4196)
-		
-		print status
-	
-		return
+    def send_message(self,message,serverSocket):
+        message = "PUT " + message
+        serverSocket.send(message)
 
+    def send_get(self,serverSocket):
+        while(1):
+            message = "GET"
+
+            serverSocket.send(message)
+
+            try:
+
+                for i in 1:2:
+                    chats = serverSocket.recv(4196)
+                    chatsArray = chats.split(" ",1)
+                    if chatsArray[0] == "USERS":
+                        print("David is gay")
+
+                    elif chatsArray[0] == "MSGS":
+                        print("David is really gay")
+
+            except Exception, e:
+                import traceback
+    		print traceback.format.exc()
+    
+    def check_status(self,status):
+        if status == "OK":
+            print "ok to chat"
+
+	else:
+            print status
+
+    def run(self):
+	self.process_command(sys.argv[1])
+        serverSocket = socket(AF_INET,SOCK_STREAM)
+	serverSocket.settimeout(.25)
+        serverSocket.connect(('localhost',self.serverPort))
+	serverSocket.send("NAME " + self.userName)
+        status = serverSocket.recv(256)
+        self.check_status(status)
+        self.send_message("Tim Jassman loves the D",serverSocket)
+        status = serverSocket.recv(256)
+        self.check_status(status)
+	self.send_get(serverSocket)
+        thread.start_new_thread(self.send_get,(serverSocket,))
+        while(1):
+            a = 0
+            
 client = ChatClient(15008)
 client.run()
+
+
+\
+
